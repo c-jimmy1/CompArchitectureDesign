5f8b31ba76b7af693594a13bf3491892a09e809b
Jimmy Chen <jc219570512@gmail.com>
2023-12-07 23:40:44 -0500

    made changes to ALU

--- design1_src/alu.sv
+++ design1_src/alu.sv
@@ -1,38 +1,32 @@
 `timescale 1ns / 1ps
+module alu(
+    input [15:0] A, B,
+    input [3:0] ALU_Sel,
+    output reg [15:0] ALU_Out,
+    output reg CarryOut
+    );
 
-module alu(input [15:0] A, B,  // ALU 16-bit Inputs
-           input [3:0] ALU_Sel, // ALU Selection
-           output reg [15:0] ALU_Out, // ALU 16-bit Output
-           output reg CarryOut // Carry Out
-         );
-   reg [15:0] ALU_Result;
-   wire [16:0] tmp;
-   assign ALU_Out = ALU_Result; // ALU out
-   assign tmp = {1'b0, A} + {1'b0, B};
-   assign CarryOut = tmp[16];
+    reg [15:0] ALU_Result;
+    wire [16:0] tmp;
 
-   always @(*) 
-   begin
-      case (ALU_Sel) 
-      4'b0000:
-         ALU_Result = A + B; // ADD
-      4'b0001:
-         ALU_Result = A - B; // SUB
-      4'b0010:
-         ALU_Result = A & B; // AND
-      4'b0011:
-         ALU_Result = A | B; // Bitwise OR
-      4'b0100:
-         ALU_Result = A ^ B; // Bitwise XOR
-      4'b0101:
-         ALU_Result = ~A;    // Bitwise NOT for A
-      4'b0110:
-         ALU_Result = ~B;    // Bitwise NOT for B
-      4'b0111:
-         ALU_Result = A << 1; // Shift Left A by 1
-      4'b1000:
-         ALU_Result = A >> 1; // Shift Right A by 1
-      default: ALU_Result = A;  // Default: Output 0
-      endcase
-   end
+    assign tmp = {1'b0, A} + {1'b0, B};
+    assign CarryOut = tmp[16];
+
+    always @* begin
+        case (ALU_Sel)
+            4'b0000: ALU_Result = A + B; // ADD
+            4'b0001: ALU_Result = A - B; // SUB
+            4'b0010: ALU_Result = A & B; // AND
+            4'b0011: ALU_Result = A | B; // OR
+            4'b0100: ALU_Result = A ^ B; // XOR
+            4'b0101: ALU_Result = ~A;    // NOT for A
+            4'b0110: ALU_Result = ~B;    // NOT for B
+            4'b0111: ALU_Result = A << 1; // Shift Left A by 1
+            4'b1000: ALU_Result = A >> 1; // Shift Right A by 1
+            default: ALU_Result = A; // Default: Output A
+        endcase
+    end
+
+    assign ALU_Out = ALU_Result;
+
 endmodule
--- design1_src/test_alu.sv
+++ design1_src/test_alu.sv
@@ -1,14 +1,16 @@
 `timescale 1ns / 1ps
+
 module alu_testbench;
-    //Inputs
+    // Inputs
     reg[15:0] A, B;
     reg[3:0] ALU_Sel;
-    //Outputs
+    // Outputs
     wire[15:0] ALU_Out;
     wire CarryOut;
 
     integer i;
     alu test_unit(A, B, ALU_Sel, ALU_Out, CarryOut);
+
     initial begin
         $dumpfile("dump.vcd");
         $dumpvars(1, alu_testbench);
@@ -16,8 +18,14 @@ module alu_testbench;
         B = 16'h02;
         ALU_Sel = 4'h0;
         for (i = 0; i < 8; i = i + 1) begin
-            ALU_Sel = ALU_Sel + 4'h1;
             #10;
+
+            // Display ALU_Out value after operation
+            $display("ALU_Out = %h for ALU_Sel = %h", ALU_Out, ALU_Sel);
+
+            // Increment ALU_Sel for the next operation
+            ALU_Sel = ALU_Sel + 4'h1;
         end
+        $finish; // End simulation after loop
     end
 endmodule
