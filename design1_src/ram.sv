5f38847a98ac0446dab172865e1c9b26fbf6ae97
j-sund <sundaj@rpi.edu>
2023-12-08 04:08:40 -0500

    Ram and test bench for ram

---
+++ design1_src/ram.sv
@@ -0,0 +1,36 @@
+// Code your design here
+`timescale 1 ns / 1 ps
+
+module ram
+  # (parameter ADDR_WIDTH = 16,
+     parameter DATA_WIDTH = 8,
+     parameter LENGTH = (1<<ADDR_WIDTH)
+    )
+
+  (   input clk,
+      input [ADDR_WIDTH-1:0] addr,
+      inout [DATA_WIDTH-1:0] data,
+      input cs,
+      input we,
+      input oe
+  );
+
+  reg [DATA_WIDTH-1:0] tmp_data;
+  reg [DATA_WIDTH-1:0] mem[LENGTH];
+
+  always @ (posedge clk) begin
+    // Used when debugging a multiplication program
+    // if (addr=='h10D) $display("Product is %d", mem[addr]);
+    if (cs & we)
+      mem[addr] <= data;
+  end
+  
+  always @ (negedge clk) begin
+    if (cs & !we)
+      tmp_data <= mem[addr];
+  end
+
+  assign data = cs & oe & !we ? tmp_data : 'hz; //changed from hz
+
+
+endmodule
